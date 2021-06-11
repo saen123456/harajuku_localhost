a8cd6d14cca1c988e622e60153d87a2dd8c2f7be
saen123456 <pipat0909737525@gmail.com>
2021-06-11 16:27:18 +0700

    add page

--- resources/views/front/voucher.blade.php
+++ resources/views/front/voucher.blade.php
@@ -415,8 +415,8 @@ require_once "header.php";
 
 
     <!-- <script src="js/jquery-2.2.4.min.js"></script> -->
-    <script src="js/common_scripts_min.js"></script>
-    <script src="js/functions.js"></script>
+    <script src="js-front/common_scripts_min.js"></script>
+    <script src="js-front/functions.js"></script>
     <script src="assets/validate.js"></script>
     <!-- Main JS -->
     <script src="assets/js/main.js"></script>
