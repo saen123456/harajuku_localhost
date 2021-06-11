6bdfa529cde44c03e729294188fff8b3652c0e2d
saen123456 <pipat0909737525@gmail.com>
2021-06-11 11:00:42 +0700

    add controller

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -7,4 +7,7 @@ use Illuminate\Http\Request;
 class ManageController extends Controller
 {
     //
+    public function index(){
+        return view('page.accept');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ManageController::class, 'index'])->name('home');
