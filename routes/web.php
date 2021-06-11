b56e3300baedc015cf5281851657a95123ef6c26
saen123456 <pipat0909737525@gmail.com>
2021-06-11 15:52:19 +0700

    add page

--- routes/web.php
+++ routes/web.php
@@ -16,56 +16,56 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ManageController::class, 'index'])->name('home');
 Route::get('/manage_money01',function(){
-    return view('page.manage_money01');
+    return view('admin.manage_money01');
 });
 Route::get('/manage_money02',function(){
-    return view('page.manage_money02');
+    return view('admin.manage_money02');
 });
 Route::get('/manage_money03',function(){
-    return view('page.manage_money03');
+    return view('admin.manage_money03');
 });
 Route::get('/manage_order01',function(){
-    return view('page.manage_order01');
+    return view('admin.manage_order01');
 });
 Route::get('/manage_order02',function(){
-    return view('page.manage_order02');
+    return view('admin.manage_order02');
 });
 Route::get('/manage_order03',function(){
-    return view('page.manage_order03');
+    return view('admin.manage_order03');
 });
 Route::get('/manage_order04',function(){
-    return view('page.manage_order04');
+    return view('admin.manage_order04');
 });
 Route::get('/manage_store01',function(){
-    return view('page.manage_store01');
+    return view('admin.manage_store01');
 });
 Route::get('/manage_store05',function(){
-    return view('page.manage_store05');
+    return view('admin.manage_store05');
 });
 Route::get('/accept', function(){
-    return view('/page/accept');
+    return view('/admin/accept');
 });
 Route::get('/manage_store01-1', function(){
-    return view('/page/manage_store01-1');
+    return view('/admin/manage_store01-1');
 });
 Route::get('/manage_store02-1', function(){
-    return view('/page/manage_store02-1');
+    return view('/admin/manage_store02-1');
 });
 Route::get('/manage_store02', function(){
-    return view('/page/manage_store02');
+    return view('/admin/manage_store02');
 });
 Route::get('/manage_store03', function(){
-    return view('/page/manage_store03');
+    return view('/admin/manage_store03');
 });
 Route::get('/manage_store04', function(){
-    return view('/page/manage_store04');
+    return view('/admin/manage_store04');
 });
 Route::get('/send', function(){
-    return view('/page/send');
+    return view('/admin/send');
 });
 Route::get('/succes', function(){
-    return view('/page/succes');
+    return view('/admin/succes');
 });
 Route::get('/test_box', function(){
-    return view('/page/test_box');
+    return view('/admin/test_box');
 });
