c19a27e2406fbb218f8c3ef8e2e919c3d14301bc
saen123456 <pipat0909737525@gmail.com>
2021-06-15 14:57:47 +0700

    chang route

--- vendor/laravel/framework/src/Illuminate/Foundation/helpers.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/helpers.php
@@ -146,6 +146,8 @@ if (! function_exists('asset')) {
         //return app('url')->asset($path, $secure);
         if($_SERVER['HTTP_HOST']=='127.0.0.1:8000')
             return app('url')->asset($path, $secure);
+        else if($_SERVER['HTTP_HOST']=='127.0.0.1:8000')
+            return app('url')->asset($path, $secure);
         else
             return app('url')->asset('public/'.$path, $secure);
     }
